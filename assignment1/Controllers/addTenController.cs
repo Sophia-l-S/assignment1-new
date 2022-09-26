019c6716be5b6f8b44765c405e1d670ee9085ce4
sophi <sophi@DESKTOP-QSB7O7V>
2022-09-26 15:11:38 -0400

    Add project files.

---
+++ assignment1/Controllers/addTenController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace assignment1.Controllers
+{
+    public class addTenController : ApiController
+    {
+        /// <summary>
+        /// takes int and adds 10
+        /// </summary>
+        /// <param name="id"> </param>
+        /// <returns>Returns 10 more than the integer input {id}.</returns>
+        /// <example>http://localhost:51950/api/addTen/10 output = 20</example>
+        public int Get(int id)
+            {
+                int difference = id + 10;
+                return difference;
+            }
+        }
+    }
+
+
---
+++ assignment1/Controllers/sqrtController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace assignment1.Controllers
+{
+    public class sqrtController : ApiController
+    {/// <summary>
+    /// 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+        public int Get(int id)
+        {
+            int difference = Math.Sqrt(id);
+            return difference;
+        }
+    }
+}
